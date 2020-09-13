2be7dfde84afef00342a40cc8458d48b57cbea45
Nichole Mattera <me@nicholemattera.gay>
2020-09-12 21:00:19 -0400

    Reverting main.go file.

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func main() {
         if req.URL.Path == "/" || req.URL.Path == "/offline" {
             data := TemplateData {
                 IncludeServiceWorker:   req.URL.Path == "/",
-                Pride:                  true,//strings.HasSuffix(req.Host, "lgbt") || strings.HasSuffix(req.Host, "gay"),
+                Pride:                  strings.HasSuffix(req.Host, "lgbt") || strings.HasSuffix(req.Host, "gay"),
             }
 
             mainTemplate.Execute(w, data)
