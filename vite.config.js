ccf05d07e2c3cc73c3b50803662a4d121dcd07e7
Nichole Mattera <me@nicholemattera.gay>
2023-01-26 07:15:50 -0600

    ✨ Added templating to the page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,62 @@
 import { defineConfig } from 'vite'
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import data from './data.json';
+
+function formatDate(dateString) {
+    const dateFormatter = new Intl.DateTimeFormat('en', { month: 'long', year: 'numeric' })
+    return dateFormatter.format(Date.UTC(...dateString.split('-').map(value => parseInt(value, 10))))
+}
+
+function formatTimeSpan(startDateString, endDateString) {
+    const [ startYear, startMonth ] = startDateString.split('-').map(value => parseInt(value, 10))
+    const [ endYear, endMonth ] = endDateString.split('-').map(value => parseInt(value, 10))
+
+    let yearSpan, monthSpan = 0
+    if (startMonth - 1 > endMonth) {
+        yearSpan = endYear - startYear - 1
+        monthSpan = 12 + endMonth - (startMonth - 1)
+    } else {
+        yearSpan = endYear - startYear
+        monthSpan = endMonth - (startMonth - 1)
+    }
+
+    let result = '';
+    if (yearSpan === 1) {
+        result += '1 year '
+    } else if (yearSpan > 1) {
+        result += `${ yearSpan } years `
+    }
+
+    if (monthSpan === 1) {
+        result += '1 month'
+    } else if (monthSpan > 1) {
+        result += `${ monthSpan } months`
+    }
+
+    return result.trimEnd()
+}
+
+function processData(data) {
+    data.experiences.forEach((experience) => {
+        experience.roles.forEach((role) => {
+            if (role.end) {
+                role.endText = formatDate(role.end)
+                role.timeSpan = formatTimeSpan(role.start, role.end)
+            }
+
+            role.isPresent = !role.end
+            role.startText = formatDate(role.start)
+        })
+    })
+
+    data.awards.forEach((award) => {
+        award.honors.forEach((honor) => {
+            honor.dateText = formatDate(honor.date)
+        })
+    })
+
+    return data;
+}
 
 export default defineConfig({
     build: {
@@ -8,7 +65,12 @@ export default defineConfig({
         outDir: '../dist'
     },
     plugins: [
-        ViteMinifyPlugin({})
+        createHtmlPlugin({
+            minify: true,
+            inject: {
+                data: processData(data)
+            }
+        })
     ],
     publicDir: '../public',
     root: 'src'
