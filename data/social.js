786ba84782c8624908b80c27b34376ae13aec203
Nichole Mattera <me@nicholemattera.gay>
2023-07-27 21:42:50 -0500

    ✨ Added new social page

---
+++ data/social.js
@@ -0,0 +1,38 @@
+import commonData from './common.json';
+
+export default {
+    ...commonData,
+    page: 'social',
+    title: 'Nichole &quot;Nickel&quot; Mattera - Social Links',
+    description: 'Social Platforms that you can find Nichole on',
+    links: [
+        {
+            title: "GitHub",
+            url: "https://github.com/NicholeMattera"
+        },
+        {
+            title: "Goodreads",
+            url: "https://www.goodreads.com/user/show/164427650-nichole-mattera"
+        },
+        {
+            title: "Instagram - Art & Photography",
+            url: "https://instagram.com/nichole.makes/"
+        },
+        {
+            title: "Instagram - Cats",
+            url: "https://instagram.com/sif_tauriel_ferdinand_and_chet/"
+        },
+        {
+            title: "Instagram - Climbing",
+            url: "https://instagram.com/nichole.climbs/"
+        },
+        {
+            title: "LinkedIn",
+            url: "https://linkedin.com/in/nicholemattera"
+        },
+        {
+            title: "Mastadon",
+            url: "https://lgbtqia.space/@im_a_nickel"
+        }
+    ]
+}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { createHtmlPlugin } from 'vite-plugin-html'
 import bookshelfData from './data/bookshelf.json';
 import commonData from './data/common.json';
 import resumeData from './data/resume.js';
+import socialData from './data/social.js';
 
 export default defineConfig({
     build: {
@@ -33,6 +34,17 @@ export default defineConfig({
                             ...commonData,
                             ...bookshelfData
                         },
+                        ejsOptions: {
+                            views: ['partials']
+                        }
+                    }
+                },
+                {
+                    entry: 'src/social.js',
+                    filename: 'social.html',
+                    template: 'social.html',
+                    injectOptions: {
+                        data: socialData,
                         ejsOptions: {
                             views: ['partials']
                         }
