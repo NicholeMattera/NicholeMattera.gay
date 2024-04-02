7343b8f4b1efb0cbab5cc219948060182626c917
Nickel Mattera <me@nicholemattera.gay>
2024-04-02 06:42:56 -0500

    🍱 Updated content

--- data/social.js
+++ data/social.js
@@ -7,52 +7,32 @@ export default {
     description: 'Social Platforms that you can find me on',
     links: [
         {
-            "title": "Final Fantasy XIV",
-            "url": "https://na.finalfantasyxiv.com/lodestone/character/12012340/"
+            title: 'GitHub',
+            url: 'https://github.com/NicholeMattera'
         },
         {
-            title: "GitHub",
-            url: "https://github.com/NicholeMattera"
+            title: 'Goodreads',
+            url: 'https://www.goodreads.com/nickelmattera'
         },
         {
-            title: "Goodreads",
-            url: "https://www.goodreads.com/user/show/164427650-nichole-mattera"
+            title: 'Instagram - Art & Photography',
+            url: 'https://instagram.com/nickel.makes/'
         },
         {
-            title: "Instagram - Art & Photography",
-            url: "https://instagram.com/nichole.makes/"
+            title: 'Instagram - Cats',
+            url: 'https://instagram.com/sif_tauriel_ferdinand_and_chet/'
         },
         {
-            title: "Instagram - Cats",
-            url: "https://instagram.com/sif_tauriel_ferdinand_and_chet/"
+            title: 'Instagram - Climbing',
+            url: 'https://instagram.com/nickel.climbs/'
         },
         {
-            title: "Instagram - Climbing",
-            url: "https://instagram.com/nichole.climbs/"
+            title: 'LinkedIn',
+            url: 'https://linkedin.com/in/nickelmattera'
         },
         {
-            title: "LinkedIn",
-            url: "https://linkedin.com/in/nicholemattera"
-        },
-        {
-            title: "Mountain Project",
-            url: "https://www.mountainproject.com/user/201597317/nichole-mattera"
-        },
-        {
-            "title": "Reddit",
-            "url": "https://www.reddit.com/user/im_a_nickel"
-        },
-        {
-            "title": "Steam",
-            "url": "https://steamcommunity.com/id/im_a_nickel/"
-        },
-        {
-            "title": "Twitch",
-            "url": "https://www.twitch.tv/im_a_nickel"
-        },
-        {
-            "title": "YouTube",
-            "url": "https://www.youtube.com/@im.a.nickel"
+            title: 'Mountain Project',
+            url: 'https://www.mountainproject.com/user/201597317/nickel-mattera'
         }
     ]
 }
